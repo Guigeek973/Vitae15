749c4475e7a3b8dd816778f0215cf798731309f8
Unknown <guillaume.ostorero@gmail.com>
2018-05-14 15:10:09 +0200

    yt

--- src/main/Initialisation.java
+++ src/main/Initialisation.java
@@ -15,7 +15,6 @@ public class Initialisation {
 		List<Client> lesClients = new ArrayList<Client>();
 		
 		ResultSet rs = Connection.getResultSetSQL("SELECT * FROM Client");
-		
 		try {
 			while (rs.next()) {
 				lesClients.add(new Client( (int)rs.getInt(0), (String)rs.getString(1), (String)rs.getString(2), (String)rs.getString(3), (String)rs.getString(4), (String)rs.getString(5) ));
