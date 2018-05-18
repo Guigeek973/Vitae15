ce13002d15ecb42dd21adb93e96aff212ca491f8
SkarkS <embarbe.raphael@gmail.com>
2018-05-18 14:01:14 +0200

    d

--- src/restaurant/GestionRestaurant.java
+++ src/restaurant/GestionRestaurant.java
@@ -2,6 +2,8 @@ package restaurant;
 
 import java.util.List;
 
+import main.Connection;
+import stock.ArticleRestaurant;
 import stock.Menu;
 
 public class GestionRestaurant {
@@ -13,12 +15,16 @@ public class GestionRestaurant {
 		this.lesMenus = lesMenus;
 		this.lesReservationsRestau = lesReservationsRestau;
 	}
+	
 	public List<Menu> getLesMenus() {
 		return lesMenus;
 	}
 	public List<ReservationRestaurant> getLesReservationsRestau() {
 		return lesReservationsRestau;
 	}
+	
+	
+	
 	public void setLesMenus(List<Menu> lesMenus) {
 		this.lesMenus = lesMenus;
 	}
@@ -29,14 +35,13 @@ public class GestionRestaurant {
 	public int getNbPetitDejJour() {
 		return 0;
 	}
-	public void addMenu(String libelle, double prix) {
-		
+	public void addMenu(String libelle, double prix, String description, List<ArticleRestaurant> lesArticles) {
+//		Connection.execSQL("INSERT INTO menu(price, label, description) VALUES (" + prix + ",'" + libelle + "', '" + description + "')");
+//		
+//		lesMenus.add(new Menu(0, "", prix, description, lesArticles));
 	}
 	public void supprimerMenu(Menu menu) {
-		
-	}
-	public void setMenu(Menu menu, double prix, String libelle) {
-		
+		Connection.execSQL("DELETE FROM menu WHERE id = " + menu.getId());
 	}
 	public Menu getMenu(int id) {
 		return null;
