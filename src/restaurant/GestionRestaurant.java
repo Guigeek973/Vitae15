0a5e9ce012a75389f770ffd77ee58250e85983ad
SkarkS <embarbe.raphael@gmail.com>
2018-05-18 15:20:04 +0200

    Menu with try catch

--- src/restaurant/GestionRestaurant.java
+++ src/restaurant/GestionRestaurant.java
@@ -1,5 +1,6 @@
 package restaurant;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import main.Connection;
@@ -37,9 +38,26 @@ public class GestionRestaurant {
 	}
 	
 
-	public void ajouterMenu(String libelle, double prix, String description, List<ArticleRestaurant> lesArticles) {
-		
+	public boolean ajouterMenu(String libelle, double prix, String description) {
+		try {
+			// SI LE LABEL DU MENU N'EXISTE PAS
+			if (!Connection.existSQL("SELECT id FROM menu WHERE label = " + libelle)) {
+				// On insert un nouveau menu dans la bdd
+				Connection.execSQL("INSERT INTO Menu VALUES ('" + libelle + "', " + prix + ", '" + description + "')");
+				// On récupère l'id de ce menu tout juste inséré
+				int idNewMenu = Connection.getResultSetSQL("SELECT id FROM menu WHERE label = " + libelle).getInt("id");
+				// On ajoute ce nouveau menu à la liste des menus
+				this.lesMenus.add(new Menu(idNewMenu, libelle, prix, description));
+				// L'ajout a fonctionné on retourne vrai
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// Si l'ajout n'a pas fonctionné on retourne faux
+		return false;
 	}
+	
 	public void supprimerMenu(Menu menu) {
 		Connection.execSQL("DELETE FROM menu WHERE id = " + menu.getId());
 	}
